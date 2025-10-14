91d572b77558062fffa595c3c5ebf886e2eaa682
ChayLim <156899992+ChayLimm@users.noreply.github.com>
2025-10-14 10:31:43 +0700

    config on apserviceprovider for dev or local testing URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 {
-    if (config('app.env') === 'develop') {
+    if (config('app.env') === 'production') {
         URL::forceScheme('https');
     }
 }
