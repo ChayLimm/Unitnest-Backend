e57490408766360377f4a72920c93b2178b9adec
ChayLim <156899992+ChayLimm@users.noreply.github.com>
2025-10-22 12:12:49 +0700

    fix docker

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,22 +11,14 @@ use Monolog\Handler\TelegramBotHandler;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, CrudTrait;
+    use HasFactory, Notifiable, SoftDeletes, CrudTrait;
 
     protected $fillable = [
         'name',
         'email',
-        'email_verified_at',
         'password',
-        'remember_token',
+        'phone',
         'role_id',
-        'telegram_id',
-        'username',
-        'phonenumber',
-        'identify_id',
-        'profile_image_url',
-        'identify_image_url',
-        'deleted_at',
     ];
 
     protected $hidden = [
