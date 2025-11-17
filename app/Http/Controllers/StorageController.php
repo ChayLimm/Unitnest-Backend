d31bd0f167a6bdf2aaffd4ae94467336edfe75be
ChayLim <156899992+ChayLimm@users.noreply.github.com>
2025-11-17 11:24:26 +0700

    fix docker

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
-
 class StorageController extends Controller
 {
     public function imageUrl(Request $request, $id)
