a83065b667f9e2d54dc9d1614979c2c77cb1a64f
ChayLim <156899992+ChayLimm@users.noreply.github.com>
2025-12-14 15:43:11 +0700

    add tenant in admin portal

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -31,6 +31,7 @@ Route::group([
     Route::crud('payment-item', 'PaymentItemCrudController');
     Route::crud('notification', 'NotificationCrudController');
     Route::crud('telegrambot', 'TelegrambotCrudController');
+    Route::crud('tenant', 'TenantCrudController');
 }); // this should be the absolute last line of this file
 
 /**
