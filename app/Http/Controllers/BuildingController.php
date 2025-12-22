ebf06b1b706b6541670af31b9ef9d012cefbd11c
Phon Sovatanak <feelingsleepy4@gmail.com>
2025-12-22 22:34:06 +0700

    Building by Landlord

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -95,7 +95,9 @@ class BuildingController extends Controller
         $perPage = $request->get('per_page', 15);
         $page = $request->get('page', 1);
 
-        $buildings = Building::paginate($perPage, ['*'], 'page', $page);
+        $buildings = Building::where('landlord_id', $landlordId)
+            ->with(['rooms.roomType', 'rooms.currentContract.tenant'])
+            ->paginate($perPage, ['*'], 'page', $page);
 
         return response()->json([
             'data' => $buildings->items(),
